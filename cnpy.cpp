bf2c65ee51dc9072315d216c77b669a054f20721
Chong Ruan <totentanz@pku.edu.cn>
2017-09-15 12:52:50 +0800

    Fix: restore header file suffix.

--- cnpy.cpp
+++ cnpy.cpp
@@ -2,7 +2,7 @@
 //Released under MIT License
 //license available in LICENSE file, or at http://www.opensource.org/licenses/mit-license.php
 
-#include"cnpy.hpp"
+#include"cnpy.h"
 #include<complex>
 #include<cstdlib>
 #include<algorithm>
